75b432852b6048a22bcc0a614c0fc6e84e16d269
Praguna Manvi <praguna18@hotmail.com>
2019-03-04 20:15:12 +0530

    Add files via upload

---
+++ models/genre.js
@@ -0,0 +1,21 @@
+var mongoose=require('mongoose');
+
+var Schema=mongoose.Schema;
+
+//Define Schema
+var GenreSchema=new Schema(
+    {
+        name:{type: String, required: true,min:3,max:100}
+    }
+    
+);
+
+//Set up a Virtual
+GenreSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports=mongoose.model('Genre',GenreSchema)
